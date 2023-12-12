5800b6a84de7a0c8a08784697b08737707fd750f
kozmickacper@gmail.com <kozmickacper@gmail.com>
2023-12-13 00:09:25 +0100

    Added chart on main page (without data)

--- SigmaTrader/Controllers/HomeController.cs
+++ SigmaTrader/Controllers/HomeController.cs
@@ -1,6 +1,43 @@
 using Microsoft.AspNetCore.Mvc;
 using SigmaTrader.Models;
 using System.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text.Json;
+
+
+/*namespace TwelveData
+{
+    public class TimeSeries
+    {
+        public Dictionary<string, string> meta { get; set; }
+        public IList<Dictionary<string, string>> values { get; set; }
+        public string status { get; set; }
+    }
+
+    class Prices
+    {
+        public static double GetPrices() { 
+            using WebClient wc = new WebClient();
+            var response = wc.DownloadString("https://api.twelvedata.com/time_series?symbol=AAPL&interval=1day&outputsize=12&apikey=25e58a5c71304cddaa4c337dd546103a");
+            var timeSeries = JsonSerializer.Deserialize<TimeSeries>(response);
+            if (timeSeries.status == "ok")
+            {
+                int[timeSeries.values.Get] prices;
+                for (int i =0; )
+                {
+
+                }
+                return "Received symbol: " + timeSeries.meta["symbol"] + ", close: " + timeSeries.values[2]["close"];
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}*/
 
 namespace SigmaTrader.Controllers
 {
@@ -15,6 +52,7 @@ namespace SigmaTrader.Controllers
 
         public IActionResult Index()
         {
+            /*ViewData["Message"] = TwelveData.Prices.GetPrices();*/
             return View();
         }
 
