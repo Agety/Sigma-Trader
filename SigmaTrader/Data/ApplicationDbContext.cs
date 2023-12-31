c19b89c7a846662732cc220201371292f652ca20
Kacper Filipek <kac.filipek.kf@gmail.com>
2023-12-31 05:34:46 +0100

    adding login and register pages BUT in progress (Imba total)

--- SigmaTrader/Data/ApplicationDbContext.cs
+++ SigmaTrader/Data/ApplicationDbContext.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using SigmaTrader.Areas.Identity.Data;
 
 namespace SigmaTrader.Data
 {
@@ -8,6 +10,19 @@ namespace SigmaTrader.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+    }
+
+    public class AuthDbContext : IdentityDbContext<SigmaTraderUser>
+    {
+        public AuthDbContext(DbContextOptions<AuthDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);  
         }
     }
 }
--- SigmaTrader/Program.cs
+++ SigmaTrader/Program.cs
@@ -1,11 +1,85 @@
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using SigmaTrader.Data;
+using SigmaTrader.Areas.Identity.Data;
 
-var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+/*var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// For production scenarios, consider keeping Swagger configurations behind the environment check
+// if (app.Environment.IsDevelopment())
+// {
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+// }
+
+app.UseHttpsRedirection();
+
+string connectionString = app.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING")!;
+
+try
+{
+    // Table would be created ahead of time in production
+    using var conn = new SqlConnection(connectionString);
+    conn.Open();
+
+    var command = new SqlCommand(
+        "CREATE TABLE Persons (ID int NOT NULL PRIMARY KEY IDENTITY, FirstName varchar(255), LastName varchar(255));",
+        conn);
+    using SqlDataReader reader = command.ExecuteReader();
+}
+catch (Exception e)
+{
+    // Table may already exist
+    Console.WriteLine(e.Message);
+}
+
+app.MapGet("/Person", () => {
+    var rows = new List<string>();
+
+    using var conn = new SqlConnection(connectionString);
+    conn.Open();
+
+    var command = new SqlCommand("SELECT * FROM Persons", conn);
+    using SqlDataReader reader = command.ExecuteReader();
+
+    if (reader.HasRows)
+    {
+        while (reader.Read())
+        {
+            rows.Add($"{reader.GetInt32(0)}, {reader.GetString(1)}, {reader.GetString(2)}");
+        }
+    }
+
+    return rows;
+})
+.WithName("GetPersons")
+.WithOpenApi();
+
+app.MapPost("/Person", (Person person) => {
+    using var conn = new SqlConnection(connectionString);
+    conn.Open();
+
+    var command = new SqlCommand(
+        "INSERT INTO Persons (firstName, lastName) VALUES (@firstName, @lastName)",
+        conn);
+
+    command.Parameters.Clear();
+    command.Parameters.AddWithValue("@firstName", person.FirstName);
+    command.Parameters.AddWithValue("@lastName", person.LastName);
+
+    using SqlDataReader reader = command.ExecuteReader();
+})
+.WithName("CreatePerson")
+.WithOpenApi(); 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
@@ -13,6 +87,43 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+
+app.Run(); */
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDefaultIdentity<SigmaTraderUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
@@ -29,6 +140,7 @@ else
 }
 
 app.UseHttpsRedirection();
+
 app.UseStaticFiles();
 
 app.UseRouting();
